614592a8164fcf43a782068f3e418f760eb1b8d0
pryabov <prpbox@gmail.com>
2015-06-14 17:58:47 +0300

    Continue work with presentation

---
+++ Presentation/Scripts/CreateWebSite.ps1
@@ -0,0 +1,4 @@
+﻿#Add-AzureAccount
+#Get-AzurePublishSettingsFile
+
+Import-AzurePublishSettingsFile "c:\Projects\SummerLabs\Hackathon-2015\GitHub\Presentation\Scripts\Windows Azure MSDN - Visual Studio Ultimate-Pay-As-You-Go-6-14-2015-credentials.publishsettings"
