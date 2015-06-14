c9060000ddb7e198ec671ebb6eb5b80e54fd76e3
pryabov <prpbox@gmail.com>
2015-06-14 19:21:18 +0300

    Work on presentation

--- Presentation/Scripts/CreateWebSite.ps1
+++ Presentation/Scripts/CreateWebSite.ps1
@@ -1,4 +1,23 @@
 ﻿#Add-AzureAccount
 #Get-AzurePublishSettingsFile
 
-Import-AzurePublishSettingsFile "c:\Projects\SummerLabs\Hackathon-2015\GitHub\Presentation\Scripts\Windows Azure MSDN - Visual Studio Ultimate-Pay-As-You-Go-6-14-2015-credentials.publishsettings"
+#Get-AzureSubscription
+#Get-AzureAccount
+
+$AzureSubscriptionName = "9a2326e6-f884-4da6-ae5b-6814d21c8575"
+
+Import-AzurePublishSettingsFile "c:\Projects\SummerLabs\Hackathon-2015\GitHub\Presentation\Scripts\Free Trial-6-14-2015-credentials.publishsettings"
+Set-AzureSubscription –SubscriptionId $AzureSubscriptionName
+
+$AzureNewWebSite = "workshop-created-from-ps"
+
+Get-AzureWebsite
+
+New-AzureWebsite $AzureNewWebSite
+Get-AzureWebsite -Name $AzureNewWebSite
+
+Stop-AzureWebsite -Name $AzureNewWebSite
+Get-AzureWebsite -Name $AzureNewWebSite
+
+Remove-AzureWebsite -Name $AzureNewWebSite
+Get-AzureWebsite
