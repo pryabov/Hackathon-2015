82c5f10a2acfa20b052a474c7c69cc060aa431ef
pryabov <prpbox@gmail.com>
2015-06-18 19:28:52 +0300

    Work with Presentation/Pavel_Ryabov__Microsoft_Azure_2015.pptx

---
+++ Example/Hackathon-WebJob/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace Hackathon_WebJob
+{
+	class Program
+	{
+		static void Main()
+		{
+			CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString);
+			CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+			CloudQueue queue = queueClient.GetQueueReference("inputtext");
+			queue.CreateIfNotExists();
+			queue.AddMessage(new CloudQueueMessage("Hello World!"));
+
+			// The connection string is read from App.config
+			JobHost host = new JobHost();
+			host.RunAndBlock();
+		}
+	}
+}
